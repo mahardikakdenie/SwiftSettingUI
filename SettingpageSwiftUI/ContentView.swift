00aae3cbbde8125f8d0a5f123b6ee31ba267f2ff
MahardiikaKD <dikamahar884@gmail.com>
2023-08-18 09:07:37 +0700

    Initial Commit

---
+++ SettingpageSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SettingpageSwiftUI
+//
+//  Created by Mahardika Kessuma Denie on 18/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SettingpageSwiftUI/SettingpageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingpageSwiftUIApp.swift
+//  SettingpageSwiftUI
+//
+//  Created by Mahardika Kessuma Denie on 18/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SettingpageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
